6ad1542ed5f0c1ced4cc513e1bb97bbb3f282e84
Kanan <kenan@luxar.se>
2021-12-02 02:58:20 +0100

    Grund Struktur
    har instalerade React för frontend och Express för backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hemlagat
+
+  "author": "Sherin Valestrand, Kanan Garaisayev, Ryan Cornelio",
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
@@ -11,3 +14,8 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
