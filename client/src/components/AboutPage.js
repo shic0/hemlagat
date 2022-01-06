7cd22710fc264f1e50ace60e6d83b60733dd926c
shic0 <72131455+shic0@users.noreply.github.com>
2022-01-06 15:59:22 +0100

    Fixes but still need pic path to be correct

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -6,13 +6,23 @@ const AboutPage = () => {
   const history = useHistory()
   return (
     <>
-    <Box height="65vh" backgroundColor="white" marginTop="30px">
+    <Box height="60vh" backgroundColor="white" marginTop="30px">
       <Typography color="black" variant="h3">Om oss</Typography>
       <Typography variant="body1">Hemlagat startade 2021, när vi en dag..</Typography>
+      <Box component="img"
+        sx={{
+          height: 233,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+        }}
+        alt="Picture of meatballs"
+        src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+      />
+    </Box>
+    
       
 
-    
-    </Box>
       <Button onClick={() => history.push('/store')} variant="outlined">Gå till Menyn!</Button> 
       <Button onClick={() => history.push('/')} variant="outlined">Back to Start</Button>
     </>
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { Box, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography, Button } from "@material-ui/core";
+import { Box, ListItemIcon, Typography, Button } from "@material-ui/core";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
-
+/* import map from '../../src/img/map.hemlagat.png'
+ */
 const Contact = () => {
   const history = useHistory()
   return (
@@ -13,12 +14,30 @@ const Contact = () => {
       Kontakt</Typography>
       <Typography>031-44 33 00</Typography>
       <Typography>hemlagat@yourdoor.com</Typography>
-      <ListItemIcon>
-        <Button href="https://www.facebook.com/Hemlagat-107368028494671"><FacebookIcon /> </Button>
-        <Button href="https://www.instagram.com/_hemlagat_/"><InstagramIcon /></Button>
-      </ListItemIcon>
-      
+     
+      <Box
+        component="img"
+        sx={{
+          height: 233,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+        }}
+        alt="The house from the offer."
+        src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+      />
+      <Box>
+        <ListItemIcon>
+          <Button href="https://www.facebook.com/Hemlagat-107368028494671">Hemlagat Finns på Facebook<FacebookIcon /> </Button>
+        </ListItemIcon>
+      </Box>
+      <Box>
+        <ListItemIcon>
+          <Button href="https://www.instagram.com/_hemlagat_/">ni hittar oss även på Instagram <InstagramIcon /></Button>
+        </ListItemIcon>
+      </Box>
     </Box>
+    
       
     <Button onClick={() => history.push('/store')} variant="outlined">Gå till Menyn!</Button> 
     <Button onClick={() => history.push('/')} variant="outlined">Back to Start</Button>
