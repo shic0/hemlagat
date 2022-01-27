955ffc54fa4a993efb62e04225458e456a162b12
shic0 <72131455+shic0@users.noreply.github.com>
2022-01-27 14:44:43 +0100

    start-sida.text Hemlagat

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -10,10 +10,15 @@ const StartPage = () => {
   return (
     <>
      <Container sx={{ mt: '1rem', maxWidth: '100vw', minHeight: 'auto'}}>
-    <Box spacing={1} marginTop="30px" textAlign="center">
-       <Typography variant= "h3">
-        Veckans Erbjudanden
-      </Typography>
+      <Box spacing={1} marginTop="30px" textAlign="center">
+        <Typography variant="h2">HEMLAGAT</Typography>
+        <Typography variant ="h2">Vi lagar all mat från grunden!</Typography>
+        <Typography style={{ maxWidth: '300px' }}variant="body2">
+          Från att skala och skära lök eller grönsaker till såsbaserna, till de färska örterna. Vi lagar allt från grunden. Alla såser som innehåller mejerier görs uteslutande på färska svenska mejerier. Och all protein du hittar på menyn kommer alltid ifrån Sverige.
+        </Typography>
+        <Typography variant= "h3">
+          Veckans Erbjudanden
+        </Typography>
       <Box display="flex" justifyContent="center"  flexDirection="column" item xs= {12} sm= {4}>
       <Grid display="flex" justifyContent="center" mt="20px" container spacing={4}>
         <Box margin="20px" item xs= {12} sm= {4}>
