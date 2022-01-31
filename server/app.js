b5f335d332278aa08f1bb6722aecea49f371e8fb
Kanan <kenan@luxar.se>
2022-01-31 13:34:24 +0100

    second

--- server/app.js
+++ server/app.js
@@ -64,7 +64,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/build/index.html'));
+  res.sendFile(path.join(__dirname+'../build/index.html'));
 });
 // --------------------------------
 
