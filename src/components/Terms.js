d41a7e3b13ce2474da415d6fa857d2d12bbd8b13
Kanan Garaisayev <72131389+Kanan11@users.noreply.github.com>
2022-02-02 21:08:15 +0100

    Update Terms.js

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -17,71 +17,10 @@ const history = useHistory()
               Annars bjuder vi på maten!
           </Typography>
         </Box>
-        <Box>
-        ALLMÄNA VILLKOR OCH BESTÄMMELSER
-Varje restaurang har specifika öppettider och erbjuder olika möjligheter i form av avhämtning. Vid lagd beställning ansvarar varje enskild restaurang själv för att denna behandlas. I Love Pizza försöker alltid upptäcka och varna om eventuella driftstörningar, men ansvarar inte för fördröjd eller utebliven leverans till följd av eventuella tekniska problem.
-
-Personuppgifter
-Se vår integritetspolicy
-
-Beställning
-All personinformation som anges i beställningsformuläret ska vara korrekt och tillhöra den som beställer produkterna. I Love Pizza ansvarar inte för utebliven leverans till följd av otillräcklig eller felaktig information i beställningsformuläret.
-
-Produkter
-I Love Pizza har som mål att alltid hålla sidan uppdaterad för eventuella förändringar i produktutbudet men vi reserverar oss för att vissa produkter kan vara slut i lager. Varje enskild restaurang har det yttersta ansvaret för kvaliteten på sina produkter. Om kunden har klagomål på enstaka produkter ska kunden i första hand ta upp detta med den enskilda restaurangen.
-
-Betalning
-Alla beställningar via I Love Pizza är bindande. Betalning ska ske i svenska kronor. Debitering av kort sker direkt vid köp.
-
-Alla priser på I Love Pizza är inklusive moms om inget annat anges. Momsen på livsmedel är 12 %. Kvitto och momsspecifikation erhålls från respektive restaurang eftersom det är dessa som säljer produkterna.
-
- Faktura & delbetalning
-I samarbete med Lindorff erbjuder vi faktura och delbetalning. Tjänsten är baserad på en engångskredit som möjliggör en enkel och säker betalning av dina köp genom faktura eller delbetalning. Mer information och fullständiga villkor om Lindorffs betalsätt hittar du på Bill.lindorff.se.
-Fakturan har en förfallotid om 14 dagar. Efter genomfört köp skickas fakturan till din folkbokföringsadress. Du kan sedan välja att betala hela beloppet med en gång eller ansöka om att dela upp betalningen i mindre delar genom att logga in på Bill.lindorff.se.
-För att kunna använda tjänsten måste du vid köptillfället vara bosatt i Sverige och minst 20 år gammal. Betalsättet är endast tillgängligt för privatpersoner och som kreditgivare förutsätter vi att du har skött dina ekonomiska åtaganden och har en god betalhistorik
-En kreditprövning görs i samband med köptillfället vilket innebär att en kreditupplysning inhämtas. Kreditupplysningar som tas av Lindorff påverkar inte din kreditvärdighet och kan inte ses av andra som begär kreditupplysning om dig, t ex banker. Vid försenad eller utebliven betalning debiteras en förseningsavgift samt dröjsmålsränta.
-För att se fullständiga villkor för Lindorffs faktura- och delbetalningstjänst se länkarna nedan:Allmänna kreditvillkor för faktura och delbetalning
-Standardiserad Europeisk konsumentkreditinformation
-3.3.2 Frågor kopplade till din faktura eller delbetalningLindorffs kundtjänst hjälper dig gärna i alla frågor som rör din faktura eller delbetalning. Kontakta oss via e-post bill@lindorff.se eller via telefon på 0771-409230, måndag – fredag kl. 08:00-17:00.
-Se också vanligt förekommande frågor och svar.
-3.3.3 Återbetalning vid faktura- eller delbetalningsköpDu som har betalt din faktura eller delbetalning men ändå ångrat ditt köp har ett tillgodohavande hos Lindorff. Lindorff återbetalar den summa du har betalat efter att vi har fått uppgift om godkänd retur från handlaren. Återbetalningen sker till ditt konto varför du behöver meddela oss dina kontouppgifter tillsammans med fakturauppgifterna.
-Kontakta oss via e-post bill@lindorff.se eller via telefon på 0771-409230, måndag – fredag kl. 08:00-17:00.
-3.3.4 Pris
-Period Ränta Månadsavgift Administrationsavgift
-3 mån 0,00% 30 kr 0 kr
-6 mån 0,00% 30 kr 95 kr
-12 mån 9,95% 30 kr 195 kr
-24 mån 19,95% 30 kr 295 kr
-
- 
-
-Hämtning
-Hämtningstiden varierar ofta med tiden på dygnet och vi reserverar oss för eventuella dröjsmål till följd av lång orderkö eller trafikproblem.
-
-Ångerrätt
-Vi beställning av livsmedel gäller ingen ångerrätt, se Distans- och hemförsäljningslagen.
-
-Missbruk
-I Love Pizza lagrar kundens IP-adress och annan värdefull information om kundens Internetleverantör för att vid eventuellt missbruk av tjänsten kunna spåra upp beställaren. Vi polisanmäler och utreder alla typer av missbruk av våra tjänster på I Love Pizza.
-
-Reklamationsrätt
-När du som kund mottagit dina beställda varor övergår ansvaret för maten till dig som kund. I Love Pizza ersätter därför inte eventuella skador på beställda varor efter tidpunkten för avhämtningen. Om du inte finns tillgänglig för att hämta upp beställda varor debiteras ordinarie priser enligt överenskommelse.
-
-Om något är fel på din beställning ber vi dig kontakta oss omedelbart via e-post info@ilovepizza.se. Så snart som möjligt efter att vi mottagit din reklamation eller åsikt granskar vi ärendet för att identifiera problemet med syfte att finna en lösning. I Love Pizza förbehåller sig rätten att avgöra vad som anses vara en lämplig lösning i varje enskild situation.
-
-Villkorsändringar
-I Love Pizza har rätt att ändra dessa villkor utan att i förväg få ditt godkännande. I Love Pizza informerar om ändringen minst en vecka innan den träder i kraft. Informationen lämnas på webbplatsen. Genom att fortsätta att nyttja I Love Pizza efter att ändringen trätt i kraft godkänner du ändringen.
-
- 
-
-Vi följer Allmänna reklamationsnämndens rekommendationer
-        </Box>
-
         <Box spacing={1} marginTop="30px" textAlign="center">
           <Button onClick={() => history.push('/store')} variant="outlined">Gå till Menyn!</Button> 
           <Button onClick={() => history.push('/')} variant="outlined">Veckans Erbjudanden</Button>
         </Box>
-        <Box>Test</Box>
     </Container>
       </>
     )
